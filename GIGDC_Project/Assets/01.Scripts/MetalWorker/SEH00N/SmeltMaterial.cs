f174b673952c796c3f6693c51fc709afd66aa82c
SEH00N <06psh0501@gmail.com>
2023-03-14 23:53:42 +0900

    기능 테스트

--- GIGDC_Project/Assets/01.Scripts/MetalWorker/SEH00N/SmeltMaterial.cs
+++ GIGDC_Project/Assets/01.Scripts/MetalWorker/SEH00N/SmeltMaterial.cs
@@ -8,7 +8,7 @@ public class SmeltMaterial : MonoBehaviour
     [SerializeField] MaterialID materialID; //아이템을 재련할 때 아이템 확인용 ID
     public MaterialID MaterialID => materialID;
 
-    private SmeltingStat currentStat = SmeltingStat.RawMaterial; //현재 재련 상태를 알려주는 변수, Product 상태일 때만 아이템에 재련 가능
+    [SerializeField] private SmeltingStat currentStat = SmeltingStat.RawMaterial; //현재 재련 상태를 알려주는 변수, Product 상태일 때만 아이템에 재련 가능
     public SmeltingStat Stat => currentStat;
 
     public void RefiningMaterail(SmeltingStat targetStat) => currentStat = targetStat; //현재 재련 상태를 바꿔주는 함수
