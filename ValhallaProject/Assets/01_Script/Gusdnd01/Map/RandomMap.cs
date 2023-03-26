beaceccaa204e58882871e134f51c62857e7c10f
Gusdnd01 <cestop42@naver.com>
2023-03-27 00:44:37 +0900

    Map

--- ValhallaProject/Assets/01_Script/Gusdnd01/Map/RandomMap.cs
+++ ValhallaProject/Assets/01_Script/Gusdnd01/Map/RandomMap.cs
@@ -6,17 +6,31 @@ public class RandomMap : MonoBehaviour
 {
     [SerializeField] private GameObject[] mapList;
     [SerializeField] private int mapListCnt = 3;
+
+    [SerializeField] private int _suffleAmount = 100;
+
     private void Awake() {
-        int randNum = 0;
+        Init();
 
         for(int i = 0; i < mapListCnt;i++){
-            randNum = UnityEngine.Random.Range(0, 6);
-            MapInstantiate(randNum, new Vector2(20 * i, 0));
+            MapInstantiate(i, new Vector2(20 * i, 0));
         }
     }
 
     public void MapInstantiate(int index, Vector2 ins_Pos){
         GameObject map_Inc = Instantiate(mapList[index]);
         map_Inc.transform.position = ins_Pos;
+    }
+    private void Init(){
+        GameObject temp;
+        int F_randNum = 0;
+        int S_randNum = 0;
+        for(int i = 0; i < _suffleAmount; i++){
+            F_randNum = Random.Range(0,6);
+            S_randNum = Random.Range(0,6);
+            temp = mapList[F_randNum];
+            mapList[F_randNum] = mapList[S_randNum];
+            mapList[S_randNum] = temp;
+        }
     }
 }
